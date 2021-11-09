8685e9852042815df65899f8f0d77dd79d4995aa
HeoManBo <iovesssr__@naver.com>
2021-11-09 11:26:54 +0900

    init

---
+++ app/src/main/java/com/example/vocabularyproject/StudyView.java
@@ -0,0 +1,14 @@
+package com.example.vocabularyproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_view);
+    }
+}
