adf9b7a16fd1a56e92d1579e84edd9e4d056ebc8
BSC <puyobsc@naver.com>
2021-11-16 20:57:08 +0900

    BSC

--- app/src/main/java/com/example/vocabularyproject/word_fragment10.java
+++ app/src/main/java/com/example/vocabularyproject/word_fragment10.java
@@ -11,10 +11,21 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class word_fragment10 extends Fragment {
+import android.speech.tts.TextToSpeech;
+import android.widget.Button;
+import java.util.Locale;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link word_fragment1#} factory method to
+ * create an instance of this fragment.
+ */
+public class word_fragment10 extends Fragment implements View.OnClickListener, TextToSpeech.OnInitListener {
     ViewGroup viewGroup;
     TextView word, mean, Example;
+    Button speak;
+    TextToSpeech tts;
+    String text;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,15 +34,62 @@ public class word_fragment10 extends Fragment {
         word = viewGroup.findViewById(R.id.Word);
         mean = viewGroup.findViewById(R.id.mean);
         Example = viewGroup.findViewById(R.id.Example);
-
-        Bundle bundle = getArguments(); //Viewpager에서 전달한 Bundle 인자 받음
-
+        speak = (Button) viewGroup.findViewById(R.id.sound);
+        Bundle bundle = getArguments(); //ViewPager가 전달한 Bundle 인자 수신
         ArrayList<String[]> arr = (ArrayList<String[]>) bundle.getSerializable("word");
         word.setText(arr.get(9)[1]);
         mean.setText(arr.get(9)[2]);
         Example.setText(arr.get(9)[3]);
 
 
+        speak.setEnabled(false);
+        speak.setOnClickListener(this);
+        text = arr.get(9)[1];
+
+        tts = new TextToSpeech(getActivity(), this);
+
         return viewGroup;
     }
+
+    private void Speak() {
+        tts.setPitch(1.0f);
+        tts.setSpeechRate(1.0f);
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1");
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int language = tts.setLanguage(Locale.ENGLISH);
+            if (language == TextToSpeech.LANG_NOT_SUPPORTED || language == TextToSpeech.LANG_MISSING_DATA) {
+                speak.setEnabled(false);
+            }
+            else {
+                speak.setEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sound :
+                Speak();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+    }
+
 }
--- app/src/main/java/com/example/vocabularyproject/word_fragment5.java
+++ app/src/main/java/com/example/vocabularyproject/word_fragment5.java
@@ -11,11 +11,21 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import android.speech.tts.TextToSpeech;
+import android.widget.Button;
+import java.util.Locale;
 
-public class word_fragment5 extends Fragment {
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link word_fragment1#} factory method to
+ * create an instance of this fragment.
+ */
+public class word_fragment5 extends Fragment implements View.OnClickListener, TextToSpeech.OnInitListener {
     ViewGroup viewGroup;
     TextView word, mean, Example;
+    Button speak;
+    TextToSpeech tts;
+    String text;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,13 +34,62 @@ public class word_fragment5 extends Fragment {
         word = viewGroup.findViewById(R.id.Word);
         mean = viewGroup.findViewById(R.id.mean);
         Example = viewGroup.findViewById(R.id.Example);
-        Bundle bundle = getArguments();
+        speak = (Button) viewGroup.findViewById(R.id.sound);
+        Bundle bundle = getArguments(); //ViewPager가 전달한 Bundle 인자 수신
         ArrayList<String[]> arr = (ArrayList<String[]>) bundle.getSerializable("word");
         word.setText(arr.get(4)[1]);
         mean.setText(arr.get(4)[2]);
         Example.setText(arr.get(4)[3]);
 
 
+        speak.setEnabled(false);
+        speak.setOnClickListener(this);
+        text = arr.get(4)[1];
+
+        tts = new TextToSpeech(getActivity(), this);
+
         return viewGroup;
     }
+
+    private void Speak() {
+        tts.setPitch(1.0f);
+        tts.setSpeechRate(1.0f);
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1");
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int language = tts.setLanguage(Locale.ENGLISH);
+            if (language == TextToSpeech.LANG_NOT_SUPPORTED || language == TextToSpeech.LANG_MISSING_DATA) {
+                speak.setEnabled(false);
+            }
+            else {
+                speak.setEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sound :
+                Speak();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+    }
+
 }
