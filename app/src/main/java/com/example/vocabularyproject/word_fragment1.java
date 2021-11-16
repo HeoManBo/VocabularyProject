9bdbcef30936ce320b8299882d67adcb1cebde90
BSC <puyobsc@naver.com>
2021-11-16 17:52:42 +0900

    Return

--- app/src/main/java/com/example/vocabularyproject/word_fragment1.java
+++ app/src/main/java/com/example/vocabularyproject/word_fragment1.java
@@ -11,6 +11,13 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import android.speech.tts.TextToSpeech;
+import android.widget.Button;
+import androidx.annotation.RequiresApi;
+import android.os.Build;
+import static android.speech.tts.TextToSpeech.ERROR;
+import java.util.Locale;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link word_fragment1#} factory method to
@@ -19,20 +26,39 @@ import java.util.ArrayList;
 public class word_fragment1 extends Fragment {
     ViewGroup viewGroup;
     TextView word, mean, Example;
+    TextToSpeech tts;
+    Button speak;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_word1,container,false);
+        super.onCreate(savedInstanceState);
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_word1, container, false);
         word = viewGroup.findViewById(R.id.Word);
         mean = viewGroup.findViewById(R.id.mean);
         Example = viewGroup.findViewById(R.id.Example);
+        speak = (Button) viewGroup.findViewById(R.id.sound);
+        speak.setEnabled(false);
+        speak.setOnClickListener(this);
         Bundle bundle = getArguments(); //ViewPager가 전달한 Bundle 인자 수신
         ArrayList<String[]> arr = (ArrayList<String[]>) bundle.getSerializable("word");
         word.setText(arr.get(0)[1]);
         mean.setText(arr.get(0)[2]);
         Example.setText(arr.get(0)[3]);
 
+        tts = new TextToSpeech(this, this);
+
+
+        speak.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                CharSequence text = arr.get(0)[1];
+                tts.setPitch(1.0f);
+                tts.setSpeechRate(1.0f);
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "word1");
+            }
+        });
 
         return viewGroup;
     }
