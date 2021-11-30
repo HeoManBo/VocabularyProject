d76973f30a6bbbe47daa0bf2621df818a9ddcc36
HeoManBo <iovesssr__@naver.com>
2021-11-30 11:38:29 +0900

    fragment완성

--- app/src/main/java/com/example/vocabularyproject/word_fragment/word_fragment12.java
+++ app/src/main/java/com/example/vocabularyproject/word_fragment/word_fragment12.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -49,15 +51,20 @@ public class word_fragment12 extends Fragment implements View.OnClickListener, T
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_word1,container,false);
-        word = viewGroup.findViewById(R.id.Word);
-        mean = viewGroup.findViewById(R.id.mean);
-        Example = viewGroup.findViewById(R.id.Example);
-        speak = (Button) viewGroup.findViewById(R.id.sound);
-        star = viewGroup.findViewById(R.id.star01);
+        return inflater.inflate(R.layout.fragment_word12, container,false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        word = view.findViewById(R.id.Word);
+        mean = view.findViewById(R.id.mean);
+        Example = view.findViewById(R.id.Example);
+        speak = (Button) view.findViewById(R.id.sound);
+        star = view.findViewById(R.id.star12);
         star.setOnClickListener(this);
-
-
 
         Bundle bundle = getArguments(); //ViewPager가 전달한 Bundle 인자 수신
         arr = (ArrayList<String[]>) bundle.getSerializable("word");
@@ -65,10 +72,12 @@ public class word_fragment12 extends Fragment implements View.OnClickListener, T
         mean.setText(arr.get(11)[2]);
         Example.setText(arr.get(11)[3]);
 
-        sentence = viewGroup.findViewById(R.id.sentence);
+        //버튼의 상태를 가져오는 함수
+        btn_load();
+
+        sentence = view.findViewById(R.id.sentence);
         sentence.setText(arr.get(11)[4]);
 
-        btn_load();
 
         speak.setEnabled(false);
         speak.setOnClickListener(this);
@@ -76,7 +85,6 @@ public class word_fragment12 extends Fragment implements View.OnClickListener, T
         text = arr.get(0)[1];
         tts = new TextToSpeech(getActivity(), this);
 
-        return viewGroup;
     }
 
     private void Speak() {
